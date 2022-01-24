57bccdad874cbc27a8e5f5f3fcf22212f355c623
Ragil14 <rahil.gilani@gmail.com>
2022-01-24 13:44:45 -0600

    added template to return data in generateMarkdown() function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-    return `![license Image] (./assets/img.png)`
-}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
     return `## License
-The license for our project is a ${license} license
-    `
+The license for our project is a ${data.license} license`
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-${renderLicenseSection(data.license)}
+## License
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Username](#username)
+* [Email](#email)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+## Username
+[${data.username}](https://www.github.com/${data.username})
+## Email
+[${data.email}](${data.email})
 `;
 }
 
